a91f5fcaa3fb05aba0e2d658f805d2640cf494c0
Kair <kair8m>
2021-02-02 13:54:51 +0300

    update: client

--- tools/client.cpp
+++ tools/client.cpp
@@ -1,8 +1,24 @@
-#include <stdio.h>
 #include <iostream>
 #include <cxxopts.hpp>
+#include <logger/Logger.h>
+#include <terminal/console.hpp>
+#include <terminal/terminal.hpp>
+#include <client/Master.h>
+#include <client/Slave.h>
+#include <client/MessageClient.h>
+
+class ClientConsole : public Console {
+
+};
+
+class ShellTerminal : public Terminal {
+
+};
 
 class TerminusClientApplication {
+private:
+  std::shared_ptr<ShellTerminal> fShellTerminal;
+  std::shared_ptr<ClientConsole> fClientConsole;
 public:
   TerminusClientApplication() {
 
